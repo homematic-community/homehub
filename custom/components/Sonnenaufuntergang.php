b1caf71c892d55cd4db340b0d84a4314b2768db2
christian1180 <126362145+christian1180@users.noreply.github.com>
2024-04-21 12:24:58 +0200

    Sonnenstand, Mondphase und Sonnenauf-/untergang von Slice eingepflegt.
    Sonnenstand, Mondphase und Sonnenauf-/untergang von Slice eingepflegt.

---
+++ custom/components/Sonnenaufuntergang.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+{
+ "component": "Sonnenaufuntergang",
+ "name": "OK - Sonne A/U",
+ "icon": "weather_sunrise.png",
+ "ise_id_sonnenaufgang":"55907",
+ "ise_id_sonnenuntergang":"55908"
+},
+ 
+*/
+function Sonnenaufuntergang($component) {
+	   if (!isset($component['color'])) $component['color'] = '#595959';
+    return '<div class="hh" style=\'border-left-color: '.$component['color'].'; border-left-style: solid;\'>'
+        . '<div class="pull-left"><img src="icon/' . $component["icon"] . '" class="icon">' . $component["name"] . '</div>'
+        . '<div class="pull-right">'
+        . '<span class="info">Sonnenaufgang:<span class="info" data-id="' . $component["ise_id_sonnenaufgang"] . '" data-component="SysVar" data-datapoint="" data-unit=""></span></span>'
+        . '<span class="info">Sonnenuntergang:<span class="info" data-id="' . $component["ise_id_sonnenuntergang"] . '" data-component="SysVar" data-datapoint="" data-unit=""></span></span>'
+        . '</div>'
+        . '<div class="clearfix"></div>'
+    . '</div>';
+}
---
+++ custom/components/Sonnenstand.php
@@ -0,0 +1,22 @@
+<?php
+/*
+{
+ "component": "Sonnenstand",
+ "name": "OK - Sonne",
+ "icon": "weather_sun.png",
+ "ise_id_azimut":"55945",
+  "ise_id_elevation":"55946"
+},
+*/
+
+function Sonnenstand($component) {
+	   if (!isset($component['color'])) $component['color'] = '#595959';
+    return '<div class="hh" style=\'border-left-color: '.$component['color'].'; border-left-style: solid;\'>'
+        . '<div class="pull-left"><img src="icon/' . $component["icon"] . '" class="icon">' . $component["name"] . '</div>'
+        . '<div class="pull-right">'
+        . '<span class="info">Sonne Azimut:<span class="info" data-id="' . $component["ise_id_azimut"] . '" data-component="SysVar" data-datapoint="4" data-unit="°"></span></span>'
+        . '<span class="info">Sonne Elevation:<span class="info" data-id="' . $component["ise_id_elevation"] . '" data-component="SysVar" data-datapoint="4" data-unit="°"></span></span>'
+        . '</div>'
+        . '<div class="clearfix"></div>'
+    . '</div>';
+}
