96aa500c267487d6ca490ea363f15d99eaceaaf0
christian1180 <126362145+christian1180@users.noreply.github.com>
2024-05-05 19:32:04 +0200

    Parameter "label eingepflegt
    Parameter "label eingepflegt. Damit können die Schaltflächen beschriftet und ausgeblendet werden.

--- components/CUX2801.php
+++ components/CUX2801.php
@@ -1,4 +1,17 @@
 <?php
+
+/*
+mit folgendem können die Schaltflächen beschriftet werden
+
+"label":"Text kurz,Text lang"
+
+ist der Text leer wird die Schaltfläche nicht angezeigt. (hier kurz)
+
+"label":",Text lang"
+
+*/
+
+
 function CUX2801($component) {
     if ($component['parent_device_interface'] == 'CUxD' && $component['visible'] == 'true' && isset($component['PRESS_SHORT'])) {
         if (!isset($component['color'])) $component['color'] = '#595959';
@@ -9,12 +22,26 @@ function CUX2801($component) {
 			else { $ShowTime = ''; }
 		}
 		else { $ShowTime = ''; }
+		if(isset($component['label']))
+		{
+			$Label = explode(",",$component['label']);
+			if($Label[0] <> "") { $LabelKurz = '<span class="set btn-text" data-set-id="' . $component['PRESS_SHORT'] . '" data-set-value="1">'.$Label[0].'</span>'; }
+			else { $LabelKurz = ''; }
+			if($Label[1] <> "") { $LabelLang = '<span class="set btn-text" data-set-id="' . $component['PRESS_LONG'] . '" data-set-value="1">'.$Label[1].'</span>'; }
+			else { $LabelLang = ''; }
+		}
+		else
+		{
+			$LabelKurz = '<span class="set btn-text" data-set-id="' . $component['PRESS_SHORT'] . '" data-set-value="1">Kurz</span>';
+			$LabelLang = '<span class="set btn-text" data-set-id="' . $component['PRESS_LONG'] . '" data-set-value="1">Lang</span>';
+		}
+			
             return '<div class="hh" style=\'border-left-color: '.$component['color'].'; border-left-style: solid;\'>'
             . '<div class="pull-left"><img src="icon/' . $component["icon"] . '" class="icon">' . $component['name'] . '</div>'
             . '<div class="pull-right">'
 			. $ShowTime
-                . '<span class="set btn-text" data-set-id="' . $component['PRESS_SHORT'] . '" data-set-value="1">Kurz</span>'
-                . '<span class="set btn-text" data-set-id="' . $component['PRESS_LONG'] . '" data-set-value="1">Lang</span>'
+                . $LabelKurz
+                . $LabelLang
             . '</div>'
             . '<div class="clearfix"></div>'
         . '</div>';
