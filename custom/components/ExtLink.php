49dfa33d0859dda976bef4cb9e582ab36b1ff29a
christian1180 <126362145+christian1180@users.noreply.github.com>
2024-07-01 23:21:30 +0200

    Update ExtLink.php
    Verhindere homematic skript trigger.

--- custom/components/ExtLink.php
+++ custom/components/ExtLink.php
@@ -5,7 +5,7 @@ function ExtLink($component) {
     return '<div class="hh" style=\'border-left-color: '.$component['color'].'; border-left-style: solid;\'>'
             . '<div class="pull-left"><img src="icon/' . $component["icon"] . '" class="icon">' . $component['name'] . '</div>'
             . '<div class="pull-right">'
-                . '<span class="run btn-action" onclick="location.href=\'' . $component['url'] . '\'">&Ouml;ffnen</span>'
+                . '<span class="btn-action" onclick="location.href=\'' . $component['url'] . '\'">&Ouml;ffnen</span>'
             . '</div>'
             . '<div class="clearfix"></div>'
         . '</div>';
