49b2543034147c20b08fd7e9558573433ace8551
christian1180 <126362145+christian1180@users.noreply.github.com>
2024-11-20 13:46:55 +0100

    HmIP_PSM_2 QHJ integriert

--- js/script.js.php
+++ js/script.js.php
@@ -140,7 +140,8 @@ var updateDatapoints = function () {
                     case 'HMIP-PS':
                     case 'HmIP-PSM-2':
                     case 'HmIP-PS-2':
-					case 'HmIP-PS-2 9YM':
+                    case 'HmIP-PS-2 9YM':
+                    case 'HmIP-PS-2 QHJ':
                     case 'HmIP-BSM':
                     case 'HmIP-BS2':
                     case 'HmIP-FSM':
