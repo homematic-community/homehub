30b4c44e67df06f1386f84a7da9abcb89d955d28
christian1180 <126362145+christian1180@users.noreply.github.com>
2024-11-21 07:22:26 +0100

    HmIP-PSM-2 QHJ korrektur

--- js/script.js.php
+++ js/script.js.php
@@ -141,7 +141,7 @@ var updateDatapoints = function () {
                     case 'HmIP-PSM-2':
                     case 'HmIP-PS-2':
                     case 'HmIP-PS-2 9YM':
-                    case 'HmIP-PS-2 QHJ':
+                    case 'HmIP-PSM-2 QHJ':
                     case 'HmIP-BSM':
                     case 'HmIP-BS2':
                     case 'HmIP-FSM':
