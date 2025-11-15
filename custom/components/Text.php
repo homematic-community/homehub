29820ced9586d87e75278e3cb02da01540e122ba
christian1180 <126362145+christian1180@users.noreply.github.com>
2025-11-15 05:44:21 +0100

    Beschreibung der Text Komponente aktualisiert.
    Beschreibung der Text Komponente aktualisiert.

--- custom/components/Text.php
+++ custom/components/Text.php
@@ -1,5 +1,10 @@
 <?php
-
+/*
+ {
+    "component": "Text",  
+    "text": "nun kommt Text oder sonstiges <img src='bildurl.jpg' alt='Beschreibung'> <br> das war's."
+ },
+*/
 function Text($component) {
     if (!isset($component['color'])) $component['color'] = '#FFCC00';    
     return '<div class="hh" style=\'border-left-color: '.$component['color'].'; border-left-style: solid;\'>'
